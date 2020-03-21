6c8ac358143056bb47ddf86d33d8af8754b779d3
Clemens Schmid <clemens@nevrome.de>
2020-03-21 19:13:09 +0100

    added logy argument to plot function

--- R/plot_RKI_timeseries.R
+++ R/plot_RKI_timeseries.R
@@ -1,23 +1,24 @@
 #' plot_RKI_timeseries
 #'
 #' @param x data.frame. RKI data as downloaded with \code{\link{get_RKI_timeseries}}
-#' @param grouping_var character. "Bundesland", "Landkreis", "Geschlecht", "Altersgruppe"
+#' @param group character. "Bundesland", "Landkreis", "Geschlecht", "Altersgruppe"
 #' @param type character. Counter type
 #' @param label logical
+#' @param logy logical
 #'
 #' @export
-plot_RKI_timeseries <- function(x, grouping_var = "Bundesland", type = "KumAnzahlFall", label = T) {
+plot_RKI_timeseries <- function(x, group = "Bundesland", type = "KumAnzahlFall", label = T, logy = F) {
 
-  x <- group_RKI_timeseries(x, !!grouping_var)
+  x <- group_RKI_timeseries(x, !!group)
   
   p <- x %>%
     tidyr::pivot_longer(cols = tidyselect::contains("Anzahl"), names_to = "Zaehler", values_to = "Anzahl") %>%
     dplyr::filter(.data[["Zaehler"]] == type) %>%
-    dplyr::mutate(label = dplyr::if_else(.data[["Meldedatum"]] == (max(x$Meldedatum) - 5*24*60*60), as.character(.data[[grouping_var]]), NA_character_)) %>%
+    dplyr::mutate(label = dplyr::if_else(.data[["Meldedatum"]] == (max(x$Meldedatum) - 5*24*60*60), as.character(.data[[group]]), NA_character_)) %>%
     ggplot2::ggplot(
       ggplot2::aes_string(
         "Meldedatum", "Anzahl", 
-        color = grouping_var, group = grouping_var,
+        color = group, group = group,
         label = "label"
       )
     ) +
@@ -31,6 +32,10 @@ plot_RKI_timeseries <- function(x, grouping_var = "Bundesland", type = "KumAnzah
     p <- p + ggrepel::geom_label_repel(
         na.rm = TRUE
       )
+  }
+  
+  if (logy) {
+    p <- p + ggplot2::scale_y_log10()
   }
 
   return(p)
