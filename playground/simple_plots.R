b9e30007acb01b67658b1c9f16c9f88f7aa86a31
Stephan Schiffels <stephan_schiffels@mac.com>
2020-03-21 21:30:23 +0100

    added two simple plotting commands to show off the package

---
+++ playground/simple_plots.R
@@ -0,0 +1,36 @@
+library(ggplot2)
+library(magrittr)
+
+dat <- get_RKI_timeseries(cache=F)
+
+group_RKI_timeseries(dat, Bundesland) %>%
+  dplyr::filter(Meldedatum > "2020-02-25") %>%
+  tidyr::drop_na(Bundesland) %>%
+  ggplot() +
+  geom_bar(mapping = aes(x = Meldedatum,
+                         y = AnzahlFall,
+                         fill = Bundesland),
+           stat = 'identity') +
+  theme_minimal() +
+  ggtitle("Gemeldete Infektionen (täglich)") +
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank())
+
+
+group_RKI_timeseries(dat, Bundesland) %>%
+  dplyr::filter(Meldedatum > "2020-02-25") %>%
+  tidyr::drop_na(Bundesland) %>%
+  dplyr::group_by(Bundesland) %>%
+  dplyr::mutate(kum_fall = cumsum(AnzahlFall)) %>%
+  dplyr::ungroup() %>%
+  ggplot() +
+  geom_area(mapping = aes(x = Meldedatum,
+                          y = kum_fall,
+                          fill = Bundesland),
+            stat = 'identity',
+            na.rm = T) +
+  theme_minimal() +
+  ggtitle("Gemeldete Infektionen (kumulativ)") +
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank())
+
