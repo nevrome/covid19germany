34695151dfb30ade31d2314d60b7bd109e59bb1e
Clemens Schmid <clemens@nevrome.de>
2020-03-21 15:54:50 +0100

    development code for data preparation

--- playground/clemens_plots.R
+++ playground/clemens_plots.R
@@ -1,1 +1,32 @@
+library(magrittr)
+
 rki <- covid19germany::get_RKI_timeseries()
+
+rki_grouped <- rki %>% 
+  dplyr::group_by(
+    Bundesland,
+    Meldedatum
+  ) %>%
+  dplyr::summarise(
+    cases = sum(AnzahlFall),
+    deaths = sum(AnzahlTodesfall)
+  ) %>%
+  dplyr::ungroup()
+
+rki_grouped_filled <- tidyr::complete(
+  rki_grouped, 
+  Bundesland = Bundesland,
+  Meldedatum = tidyr::full_seq(Meldedatum, 24*60*60),
+  fill = list(cases = 0, deaths = 0)
+)
+
+rki_grouped_filled_cum <- rki_grouped_filled %>%
+  dplyr::group_by(Bundesland) %>%
+  dplyr::mutate(
+    cumcases = cumsum(cases),
+    cumdeaths = cumsum(deaths)
+  ) %>%
+  dplyr::ungroup()
+
+
+
