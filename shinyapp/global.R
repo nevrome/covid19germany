cae427b7eb9b5533c96108b32e0adfb2dc09c2c1
joboog <johannesboog@yahoo.de>
2020-03-22 11:01:49 +0100

    [ShinyApp] Check and install required packages.

--- shinyapp/global.R
+++ shinyapp/global.R
@@ -6,6 +6,10 @@
 
 
 # r environment -----------------------------------------------------------
+if (!("shiny" %in% rownames(installed.packages()))) install.packages("shiny")
+if (!("tidyverse" %in% rownames(installed.packages()))) install.packages("tidyverse")
+if (!("lubridate" %in% rownames(installed.packages()))) install.packages("lubridate")
+
 library(shiny)
 library(tidyverse)
 library(lubridate)
