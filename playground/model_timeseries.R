f0a97a80f19e2f94a460063a18b1e480d794736c
Moritz Aschoff <maschoff@team.mobile.de>
2020-03-22 11:19:43 +0100

    added predictions for n future days

--- playground/model_timeseries.R
+++ playground/model_timeseries.R
@@ -1,5 +1,6 @@
 ## globals:
 MIN.CASES=5
+N.DAYS.FUTURE=7
 
 ## read data:
 df = get_RKI_timeseries()
@@ -21,11 +22,18 @@ for (this.ag in ag.split){
   this.ag = this.ag[order(this.ag$Meldedatum),]
   this.ag[,"x.log"]=log(this.ag[,"x"])
   mdl=lm("x.log ~ Meldedatum",data=this.ag)
-  this.ag[,"x.pred"]=exp(predict(mdl,data=this.ag))
-  ## TODO: predict N days into the future
-  
+
+  ## append N.DAYS.FUTURE to data
+  n=(nrow(this.ag)+1)
+  last.date=as.Date(max(this.ag$Meldedatum))
+  this.ag[n:(n+N.DAYS.FUTURE-1),]=NA
+  this.ag[n:(n+N.DAYS.FUTURE-1),"Meldedatum"]=seq(last.date+1,by=1,length.out=N.DAYS.FUTURE)
+
+  ## predict on all dates (including future dates)
+  this.ag[,"x.pred"]=exp(predict(mdl,newdata=this.ag))
+
   ## plot data and prediction
-  ymax=max(this.ag[,c("x.pred","x")])
+  ymax=max(this.ag[,c("x.pred","x")],na.rm=TRUE)
   plot(this.ag[,"Meldedatum"],this.ag[,"x"],type="b",col="black",pch=20,main=this.region,xlab="date",ylab="cases",ylim=c(0,ymax))
   points(this.ag[,"Meldedatum"],this.ag[,"x.pred"],type="b",col="red")
 }
