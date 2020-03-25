a4425a87dab32ae15034f3e0b9564ccb7e5e0d4d
Clemens Schmid <clemens@nevrome.de>
2020-03-25 11:54:01 +0100

    some experiments to be discussed with colleagues

--- playground/clemens_plots.R
+++ playground/clemens_plots.R
@@ -6,6 +6,34 @@ rki_timeseries_landkreis <- group_RKI_timeseries(rki, "Landkreis")
 
 plot_RKI_timeseries(rki, "Geschlecht", "KumAnzahlTodesfall", logy = T)
 
+
+####
+
+library(magrittr)
+library(covid19germany)
+library(ggplot2)
+
+rki <- covid19germany::get_RKI_timeseries()
+de <- rki %>%
+  group_RKI_timeseries() %>%
+  dplyr::mutate(
+    A_estimation_of_cases_from_later_deaths = c(KumAnzahlTodesfall[17:(length(KumAnzahlTodesfall))], rep(NA, 16)) * 100,
+    B_estimation_of_cases_from_A = c(rep(NA, 16), A_estimation_of_cases_from_later_deaths[1:(length(A_estimation_of_cases_from_later_deaths) - 16)]) * 8 
+  ) %>%
+  dplyr::select(-AnzahlFall, -AnzahlTodesfall) %>%
+  tidyr::pivot_longer(cols = c("KumAnzahlFall", "KumAnzahlTodesfall", "A_estimation_of_cases_from_later_deaths", "B_estimation_of_cases_from_A"), names_to = "count")
+
+de %>% ggplot() +
+  geom_bar(
+    ggplot2::aes(
+      Meldedatum, value,
+      fill = count
+    ),
+    stat = "identity",
+    position = "dodge"
+  ) +
+  theme_minimal()
+
 ####
 
 library(covid19germany)
