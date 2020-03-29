8d48a7df8e90b94e1d3cd906b90cc27b85fd37e5
Clemens Schmid <clemens@nevrome.de>
2020-03-29 15:47:07 +0200

    condensing the plots

--- playground/clemens_blog_post.R
+++ playground/clemens_blog_post.R
@@ -3,12 +3,12 @@ library(ggplot2)
 
 rki <- covid19germany::get_RKI_timeseries(cache = F) %>% 
   dplyr::filter(
-    Date >= lubridate::as_datetime("2020-02-24")
+    Date >= lubridate::as_datetime("2020-03-02")
   )
 
-rki_grouped <- group_RKI_timeseries(rki, Bundesland)
+rki_grouped <- covid19germany::group_RKI_timeseries(rki, Bundesland)
 
-rki_grouped %>%
+p1a <- rki_grouped %>%
   ggplot() +
   geom_area(
     aes(Date, CumNumberDead, fill = Bundesland, group = Bundesland),
@@ -16,23 +16,10 @@ rki_grouped %>%
   ) +
   scale_y_continuous(labels = scales::comma) +
   ggtitle("Cumulative number of deaths by Bundesland") + ylab("") + xlab("") +
-  theme_minimal() + theme(axis.text = element_text(size = 12))
+  theme_minimal() + theme(axis.text = element_text(size = 12)) +
+  guides(fill = FALSE)
 
-ggsave("p1.png", plot = last_plot(), "png", "~/Desktop/covid19/", scale = 3, width = 10, height = 4, units = "cm")
-
-rki_grouped %>%
-  ggplot() +
-  geom_area(
-    aes(Date, NumberNewDead, fill = Bundesland, group = Bundesland),
-    color = "white", size = 0.3
-  ) +
-  scale_y_continuous(labels = scales::comma) +
-  ggtitle("Number of new daily deaths by Bundesland") + ylab("") + xlab("") +
-  theme_minimal() + theme(axis.text = element_text(size = 12))
-
-ggsave("p2.png", last_plot(), "png", "~/Desktop/covid19/", scale = 3, width = 10, height = 4, units = "cm")
-
-rki_grouped %>%
+p1b <- rki_grouped %>%
   ggplot() +
   geom_area(
     aes(Date, CumNumberTestedIll, fill = Bundesland, group = Bundesland),
@@ -40,11 +27,23 @@ rki_grouped %>%
   ) +
   scale_y_continuous(labels = scales::comma) +
   ggtitle("Cumulative number of tested, positive cases by Bundesland") + ylab("") + xlab("") +
-  theme_minimal() + theme(axis.text = element_text(size = 12))
+  theme_minimal() + theme(axis.text = element_text(size = 12), legend.title = element_blank())
 
-ggsave("p3.png", last_plot(), "png", "~/Desktop/covid19/", scale = 3, width = 10, height = 4, units = "cm")
+p1 <- cowplot::plot_grid(p1a, p1b, align = "h", ncol = 2, rel_widths = c(1, 1.5))
+cowplot::ggsave2("p1.png", p1, "png", "~/Desktop/covid19/", scale = 3, width = 10, height = 4, units = "cm")
 
-rki_grouped %>%
+p2a <- rki_grouped %>%
+  ggplot() +
+  geom_area(
+    aes(Date, NumberNewDead, fill = Bundesland, group = Bundesland),
+    color = "white", size = 0.3
+  ) +
+  scale_y_continuous(labels = scales::comma) +
+  ggtitle("Number of new daily deaths by Bundesland") + ylab("") + xlab("") +
+  theme_minimal() + theme(axis.text = element_text(size = 12)) +
+  guides(fill = FALSE)
+
+p2b <- rki_grouped %>%
   ggplot() +
   geom_area(
     aes(Date, NumberNewTestedIll, fill = Bundesland, group = Bundesland),
@@ -52,9 +51,10 @@ rki_grouped %>%
   ) +
   scale_y_continuous(labels = scales::comma) +
   ggtitle("Number of daily tested, positive cases by Bundesland") + ylab("") + xlab("") +
-  theme_minimal() + theme(axis.text = element_text(size = 12))
+  theme_minimal() + theme(axis.text = element_text(size = 12), legend.title = element_blank())
 
-ggsave("p4.png", last_plot(), "png", "~/Desktop/covid19/", scale = 3, width = 10, height = 4, units = "cm")
+p2 <- cowplot::plot_grid(p2a, p2b, align = "h", ncol = 2, rel_widths = c(1, 1.5))
+cowplot::ggsave2("p2.png", p2, "png", "~/Desktop/covid19/", scale = 3, width = 10, height = 4, units = "cm")
 
 #### map ####
 
