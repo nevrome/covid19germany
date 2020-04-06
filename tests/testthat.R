535b3c829191aa41d2bd71b72992107efc80dd2b
Clemens Schmid <clemens@nevrome.de>
2020-04-06 22:25:23 +0200

    very, very basic tests

---
+++ tests/testthat.R
@@ -0,0 +1,5 @@
+library(testthat)
+library(covid19germany)
+library(magrittr)
+
+test_check("covid19germany")
---
+++ tests/testthat/test-basic_pipeline.R
@@ -0,0 +1,27 @@
+context("test of all functions with an arbitrary setting")
+
+rki_spatial <- get_RKI_spatial("Bundesland")
+rki_timeseries <- get_RKI_timeseries()
+
+test_that("everything works at least generally", {
+  expect_s3_class(
+    rki_spatial, 
+    "sf"
+  )
+  expect_s3_class(
+    rki_timeseries,
+    "data.frame"
+  )
+  expect_s3_class(
+    rki_timeseries %>% group_RKI_timeseries(),
+    "data.frame"
+  )
+  expect_silent(
+    rki_timeseries %>% plot_RKI_timeseries()
+  )
+  expect_s3_class(
+    rki_timeseries %>% estimatepast_RKI_timeseries(., prop_death = 0.01, mean_days_until_death = 17, doubling_time = 10),
+    "data.frame"
+  )
+})
+
