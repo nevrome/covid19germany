d6b75f7257e30e01792b4c77bc45176a3ca36a75
Peter Steinbach <p.steinbach@hzdr.de>
2020-04-07 15:01:26 +0200

    unit test added

---
+++ tests/testthat/test-raw-vs-filtered.R
@@ -0,0 +1,22 @@
+context("test of all functions with an arbitrary setting")
+
+rki_timeseries <- get_RKI_timeseries()
+raw_timeseries <- get_RKI_timeseries(raw=T)
+
+test_that("raw downloads give something else than filtered downloads", {
+  expect_s3_class(
+    rki_timeseries,
+    "data.frame"
+  )
+  expect_s3_class(
+    raw_timeseries,
+    "data.frame"
+  )
+  expect(
+    length(rki_timeseries) <= length(raw_timeseries),
+    sprintf("rki_timeseries has length %i !<= %i from raw.",
+            length(rki_timeseries),
+            length(raw_timeseries))
+  )
+})
+
