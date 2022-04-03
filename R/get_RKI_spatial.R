eaa81a3e174831a9d0dae5dc6c51bd17c6b53e48
Clemens Schmid <clemens@nevrome.de>
2022-04-03 19:58:45 +0200

    another name change in the spatial data table

--- R/get_RKI_spatial.R
+++ R/get_RKI_spatial.R
@@ -69,7 +69,7 @@ download_RKI_spatial <- function(url, resolution) {
   utils::unzip(shape_zip, exdir = unzip_dir)
   
   if (resolution == "Bundesland") {
-    sf_object <- sf::read_sf(file.path(unzip_dir, "RKI_Corona_Bundesl%C3%A4nder.shp")) %>% 
+    sf_object <- sf::read_sf(file.path(unzip_dir, "BL_mit_EW_und_Faellen.shp")) %>% 
       dplyr::mutate(
         Bundesland = .data[["LAN_ew_GEN"]]  
       )
